fd1432701f6809fc134d333113d6d04f28228f08
alsgudrl132 <alsgudrl132@gmail.com>
2025-06-18 18:45:14 +0900

    lcd 파일 생성

---
+++ elevator/Core/Inc/lcd.h
@@ -0,0 +1,15 @@
+/*
+ * lcd.h
+ *
+ *  Created on: Jun 18, 2025
+ *      Author: psh
+ */
+
+#ifndef INC_LCD_H_
+#define INC_LCD_H_
+
+#include "main.h"
+
+uint8_t a = 0;
+
+#endif /* INC_LCD_H_ */
---
+++ elevator/Core/Src/lcd.c
@@ -0,0 +1,9 @@
+/*
+ * lcd.c
+ *
+ *  Created on: Jun 18, 2025
+ *      Author: psh
+ */
+
+#include "lcd.h"
+
