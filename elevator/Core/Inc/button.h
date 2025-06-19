fe35aee0036e191bd2ff062fafccaf188942a935
kimdo1006 <doyeon03030@gmail.com>
2025-06-19 09:55:45 +0900

    button 파일 생성

---
+++ elevator/Core/Inc/button.h
@@ -0,0 +1,13 @@
+/*
+ * button.h
+ *
+ *  Created on: Jun 19, 2025
+ *      Author: user4
+ */
+
+#ifndef INC_BUTTON_H_
+#define INC_BUTTON_H_
+
+#include "main.h"
+
+#endif /* INC_BUTTON_H_ */
---
+++ elevator/Core/Src/button.c
@@ -0,0 +1,9 @@
+/*
+ * button.c
+ *
+ *  Created on: Jun 19, 2025
+ *      Author: user4
+ */
+
+
+#include "button.h"
