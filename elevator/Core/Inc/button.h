c70f58dc9200512ed767e288718cb99b93ae2df7
kimdo1006 <doyeon03030@gmail.com>
2025-06-19 11:44:09 +0900

    버튼 핀 지정, 층수 디바운싱

--- elevator/Core/Inc/button.h
+++ elevator/Core/Inc/button.h
@@ -1,13 +1,22 @@
-/*
- * button.h
- *
- *  Created on: Jun 19, 2025
- *      Author: user4
- */
-
 #ifndef INC_BUTTON_H_
 #define INC_BUTTON_H_
 
 #include "main.h"
+#include "stdbool.h"
+
+typedef struct
+{
+
+	GPIO_TypeDef		*port;
+	uint16_t			pinNumber;
+	GPIO_PinState		onState;
+
+
+
+}BUTTON_CONTROL;
+
+
+
+bool buttonGetPressed(uint8_t num);
 
 #endif /* INC_BUTTON_H_ */
--- elevator/Core/Src/button.c
+++ elevator/Core/Src/button.c
@@ -1,9 +1,45 @@
-/*
- * button.c
- *
- *  Created on: Jun 19, 2025
- *      Author: user4
- */
-
-
 #include "button.h"
+
+BUTTON_CONTROL button[3]=
+{
+		{GPIOC, GPIO_PIN_8, 0},
+		{GPIOC, GPIO_PIN_6, 0},
+		{GPIOC, GPIO_PIN_5, 0}, //여기까지 1~3층
+		{GPIOC, GPIO_PIN_9, 0},
+		{GPIOB, GPIO_PIN_8, 0}	// 여기까지 위아래
+
+
+};
+
+
+bool  buttonGetPressed(uint8_t num)
+{
+	static uint32_t prevTime[3] = {0xffffffff};
+
+	if(prevTime[num] == 0xffffffff)
+	{
+		prevTime[num] = HAL_GetTick();
+	}
+
+	bool ret = false;
+
+
+	if(HAL_GPIO_ReadPin(button[num].port, button[num].pinNumber) == button[num].onState)
+	{
+		uint32_t currTime = HAL_GetTick();
+
+		if(currTime - prevTime[num] > 30)
+		{
+			if(HAL_GPIO_ReadPin(button[num].port, button[num].pinNumber) == button[num].onState)
+			{
+				ret =true;
+			}
+			prevTime[num] = currTime;
+		}
+
+	}
+
+	return ret;
+
+
+}
