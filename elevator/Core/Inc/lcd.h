c43de432b242c49a411b3712ac5413e6706dcb99
alsgudrl132 <alsgudrl132@gmail.com>
2025-06-19 15:36:00 +0900

    lcd 브랜치에서 lcd.c와 lcd.h 파일만 가져옴

---
+++ elevator/Core/Inc/lcd.h
@@ -0,0 +1,30 @@
+#ifndef INC_LCD_H_
+#define INC_LCD_H_
+
+#include "main.h"
+#include "string.h"
+
+/*
+ * I2C LCD는 4bit 통신
+ */
+
+#define I2C_LCD_ADDRESS   (0x27<<1)
+#define BACKLIGHT_ON      0x08
+
+/* LCD Command */
+#define DISPLAY_ON    0x0C
+#define DISPLAY_OFF   0x08
+#define CLEAR_DISPLAY 0x01      // delay 2ms
+#define RETURN_HOME   0x02
+
+extern const char *state[];
+extern const char *prefix[];
+
+void lcdCommand(uint8_t command);
+void lcdData(uint8_t data);
+void i2cLcd_Init();
+void lcdString(char *str);
+void moveCursor(uint8_t row, uint8_t col);
+char* returnLcdString(const char *prefixStr, uint8_t stateIndex, uint8_t floor, uint8_t option);
+void lcdStart(const char *upperPrefix, uint8_t stateIndex, uint8_t floor);
+#endif /* INC_LCD_H_ */
